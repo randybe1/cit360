c46e558fae66e3e15e1650503516881f9ba0b276
Randy ther Master <Randy ther Master@LAPTOP-K46JI4VR>
2018-05-12 20:57:18 -0600

    List Collection Interface

---
+++ JavaApplication4/src/MyCollectionList.java
@@ -0,0 +1,40 @@
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Randy ther Master
+ */
+public class MyCollectionList {
+    //Example of ArrayList
+   public static void main(String a[]){
+  ArrayList<String> listStrings = new ArrayList<String>();
+  listStrings.add("One");
+  listStrings.add("Two");
+  listStrings.add("Three");
+  listStrings.add("Four");
+  System.out.println(listStrings);
+   }
+}
+public class MyCollectionLinkedList{
+    //Example of LinkedList
+ public static void main(String a[]){
+  LinkedList<String> listStrings = new LinkedList<String>();
+  listStrings.add("Five");
+  listStrings.add("Six");
+  listStrings.add("Seven");
+  listStrings.add("Eight");
+  System.out.println(listStrings);
+}
+}
+
+
+    
+
