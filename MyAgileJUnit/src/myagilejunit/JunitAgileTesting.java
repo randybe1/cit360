5b74d97b5ec9441652b5683075b33b25433a8b7f
Randy Beckford <Randy Beckford@Randy>
2018-06-17 01:30:27 -0600

    Created Agile Method for JUnit.

---
+++ MyAgileJUnit/src/myagilejunit/JunitAgileTesting.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package myagilejunit;
+
+/**
+ *
+ * @author Randy Beckford
+ */
+public class JunitAgileTesting {
+     public int add(String s1, String s2){
+        int int1 = Integer.parseInt(s1);
+        int int2 = Integer.parseInt(s2);
+        return int1 + int2;
+     }
+}
