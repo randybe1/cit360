05715b8e334f8a58ea886cd89687f99b826840ac
Randy ther Master <Randy ther Master@LAPTOP-K46JI4VR>
2018-07-16 01:09:35 -0600

    Thread practice 2

---
+++ ThreadRunnables/src/threadrunnables/ThreadsPractice2.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package threadrunnables;
+
+/**
+ *
+ * @author Randy ther Master
+ */
+class Threads extends Thread {
+	
+	public void run(){
+		for(int i=1; i <=10; i++){
+			System.out.println(Thread.currentThread().getId() + " This is a Thread");
+		}
+	}
+}
+
+public class ThreadsPractice2 {
+	
+	public static void main(String[] args) {
+		Threads thread_1 = new Threads();
+		thread_1.start();
+
+		Threads thread_2 = new Threads();
+		thread_2.start();
+	}
+}
