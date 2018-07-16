3082591eee7401131fa3fed8676846948a767c35
Randy Beckford <Randy Beckford@Randy>
2018-07-15 22:03:47 -0600

    interface for the controller

---
+++ JavaApplication1/src/Calculate/doMath.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Calculate;
+
+/**
+ *
+ * @author Randy Beckford
+ */
+public interface doMath {
+    void execute(Integer input1, Integer input2);
+}
