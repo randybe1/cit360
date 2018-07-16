d2d9b54e41e428edca511a3c85a031956c9b2586
Randy Beckford <Randy Beckford@Randy>
2018-07-16 09:53:25 -0600

    JUnit Agile Testing IT

--- MyAgileJUnit/test/myagilejunit/JunitAgileTestingIT.java
+++ MyAgileJUnit/test/myagilejunit/JunitAgileTestingIT.java
@@ -73,4 +73,32 @@ public class JunitAgileTestingIT {
         int result = instance.add(s1, s2);
         
     }
+    @Test
+	public void testMultiply() {
+		Multiplication test = new Multiplication();
+		int multiplication = test.multiplication(2, 5);
+		assertEquals(10, multiplication);
+	}
+	
+	@Test
+	public void testSubtract() {
+		Subtraction test = new Subtraction();
+		int subtraction = test.subtraction(2, 2);
+		assertEquals(5, subtraction);
+	}
+	
+	@Test
+	public void testDivision() {
+		Division test = new Division();
+		int division = test.division(2,2);
+		assertEquals(1, division);
+	}
+	
+	@Test
+	public void testAddition() {
+		Addition test = new Addition();
+		int division = test.addition(2,2);
+		assertEquals(4, division);
+    
+}
 }
